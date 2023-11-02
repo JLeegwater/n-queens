b7adf2565839ec68979cfde20d6f7113d2eef93d
Jleegwater <Jleegwater@live.com>
2023-11-01 19:03:40 -0700

    made a queen img and fixed the tile# absolute

--- app/pages/chessboard.js
+++ app/pages/chessboard.js
@@ -15,10 +15,12 @@ function reducer(state, action) {
 
 			// check if were placing or deleting
 			const setOrDeset = queens[row] == -1;
+
+			// if theres no queen at our tile, then set the queen
 			if (!setOrDeset) {
 				col = queens[row];
 			}
-			// if placing set the col, if deleting set -1
+			// if placing, set the col, if deleting set to -1
 			newQueens[row] = setOrDeset ? col : -1;
 
 			// Update taken arrays
@@ -44,13 +46,21 @@ export function Chessboard({ numQueens = 8 }) {
 	const squareSize = 100 / numQueens;
 	const [isOpen, setIsOpen] = useState(false);
 	const initialState = {
+		// array size is how many rows there are and then the integer stored is what tile in that row is the one selected
 		queens: Array(numQueens).fill(-1),
+
+		// store what columns are attacked, so we dont have to search through the array
 		takenColumns: Array(numQueens).fill(false),
+
+		// store what diagonals are being attacked
 		takenDiagonals: Array(numQueens * 2 - 1).fill(false),
+
+		// store what anitidiagonals(other way of the last one) are being attacked
 		takenAntidiagonals: Array(numQueens * 2 - 1).fill(false),
 	};
 	const [state, dispatch] = useReducer(reducer, initialState);
 
+	// call countQueens after state changes
 	useEffect(() => {
 		countQueens();
 	}, [state]);
@@ -58,6 +68,7 @@ export function Chessboard({ numQueens = 8 }) {
 	const isQueen = (row, col) => {
 		return state.queens[row] == col ? true : false;
 	};
+
 	const isTileAttacked = (row, col) => {
 		// Check for attacks in verticals
 		if (state.takenColumns[col]) {
@@ -70,6 +81,7 @@ export function Chessboard({ numQueens = 8 }) {
 		}
 
 		// Check for attacks in diagonals
+		// calculate what diagonal the tile belongs to
 		const diagonalDiff = numQueens - 1 - row + col;
 		const antidiagonalSum = row + col;
 
@@ -83,16 +95,21 @@ export function Chessboard({ numQueens = 8 }) {
 
 		return false;
 	};
+
+	// count how many queens are placed and compare it to our board size
 	const countQueens = () => {
 		let count = 0;
 		state.queens.forEach((element) => {
 			element != -1 && count++;
 		});
+		// open congrats popup if we've won
 		count == numQueens && setIsOpen(true);
 		return count;
 	};
 
 	const onClick = (row, col) => {
+		// check if our tile is valid or a queen
+		// if its a queen we dispatch and it will unset the queen
 		if (!isTileAttacked(row, col) || isQueen(row, col)) {
 			dispatch({
 				type: "MOVE_QUEEN",
@@ -107,7 +124,7 @@ export function Chessboard({ numQueens = 8 }) {
 		<div className="w-full max-w-full flex flex-col mx-auto border-black ">
 			{isOpen && (
 				<div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-auto">
-					<div className="bg-white p-6 rounded-lg text-black">
+					<div className="bg-gray-500 p-6 rounded-lg text-black">
 						<h2 className="text-lg font-medium">Congratulations!</h2>
 
 						<p className="mt-4">You found a solution! Well done.</p>
@@ -139,11 +156,12 @@ export function Chessboard({ numQueens = 8 }) {
 									width: `${squareSize}vw`,
 								}}
 							>
-								<span className="absolute z-20">
-									{colIndex + rowIndex * numQueens}
-								</span>
-								{state.queens[rowIndex] === colIndex && (
-									<div className="queen"></div>
+								{state.queens[rowIndex] !== colIndex ? (
+									<span>{colIndex + rowIndex * numQueens}</span>
+								) : (
+									state.queens[rowIndex] === colIndex && (
+										<div className="queen"></div>
+									)
 								)}
 							</div>
 						);
